e1d5bf96f97673ce01dac7bf1e71a2f5d6e61dc6
octacian <enduffy2014@outlook.com>
2017-04-12 10:52:27 -0700

    Add gamemode API
    Documented in mods/eden/API.md.

--- mods/eden/API.md
+++ mods/eden/API.md
@@ -5,3 +5,22 @@ The Eden mod API provides a single function to get information about the current
 `eden.get_version()`
 
 * Returns a table containing fields `version`, `type` (release type), `core` (corresponding Minetest core version), `core_type` (correspending release type of Minetest core).
+
+`eden.register_gamemode(name, def)`
+
+* Register a new gamemode for use with `/gamemode` chatcommand
+* `name`: Name for gamemode
+* `def`: See [#Gamemode definition]
+
+`eden.set_gamemode(player, gamemode)`
+
+* Set the gamemode of a player
+* Returns `true` if successful, otherwise, gamemode does not exist
+* `player`: PlayerRef
+* `gamemode`: Name of gamemode
+
+`eden.get_gamemode(player)`
+
+* Get the gamemode of a player
+* If the player has no gamemode set, it will be automatically set depending on the `creative_enabled` setting in `minetest.conf`
+* `player`: PlayerRef (or player name)
--- mods/eden/chatcommands.lua
+++ mods/eden/chatcommands.lua
@@ -22,3 +22,36 @@ minetest.register_chatcommand("heal", {
     return true, player:get_player_name().." had been healed"
   end,
 })
+
+-- [privelege] Gamemode
+minetest.register_privilege("gamemode", "Ability to use /gamemode")
+
+-- [command] Gamemode - /gamemode <mode>
+minetest.register_chatcommand("gamemode", {
+  description = "Change gamemode",
+  params = "<gamemode> | <name> <gamemode>",
+  privs = {gamemode=true},
+  func = function(name, param)
+		local params = param:split(" ")
+		local player = minetest.get_player_by_name(name)
+		local newgm
+
+		if params and #params == 1 then
+			newgm = params[1]
+		elseif params and #params == 2 then
+			if minetest.get_player_by_name(params[1]) then
+				player = minetest.get_player_by_name(params[1])
+				newgm  = params[2]
+			end
+		else
+			return false, "Invalid usage (see /help gamemode)"
+		end
+
+		-- Set gamemode
+		if eden.set_gamemode(player, newgm) then			
+	    return true, "Set "..player:get_player_name().."'s gamemode to "..param
+		else -- else, Return invalid gamemode
+			return false, "Invalid gamemode: "..newgm
+		end
+  end,
+})
