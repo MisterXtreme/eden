c15f341a760089c5931c603ab9004989684873a4
octacian <enduffy2014@outlook.com>
2017-06-01 14:07:43 -0700

    Trees: Split registrations into separate file

---
+++ mods/trees/trees.lua
@@ -0,0 +1,23 @@
+-- trees/trees.lua
+
+---
+--- Tree Registrations
+---
+
+trees.register("oak", {
+  basename = "Oak",
+  center = "trees_oak_log.png",
+  sides = "trees_oak_log_sides.png",
+	plank = "trees_oak_plank.png",
+  sapling = "trees_oak_sapling.png",
+  leaf = "trees_oak_leaf.png",
+  offset = { x = -3, y = -1, z = -3},
+  schematic = "oak_tree.mts",
+  mapgen = {
+    place_on = {"soil:dirt_with_grass"},
+    fill_ratio = 0.05,
+    biomes = {"eden:grassland"},
+    y_min = 1,
+    y_max = 200,
+  },
+})
