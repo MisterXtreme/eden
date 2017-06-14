468aefefca4d2e5fbb4ad0e90b784a65db1b7f83
octacian <enduffy2014@outlook.com>
2017-06-14 15:18:48 -0700

    Tools: Add steel toolset

--- mods/tools/README.md
+++ mods/tools/README.md
@@ -9,3 +9,10 @@ Authors of media (textures, models, and sounds)
 -----------------------------------------------
 Everything not listed in here:
 XtremeHacker (CC BY-SA 3.0)
+
+KaadmY (CC BY-SA 3.0)
+	tools_steel_axe.png
+	tools_steel_shovel.png
+	tools_steel_pick.png
+	tools_steel_hoe.png
+	tools_steel_sword.png
--- mods/tools/tools.lua
+++ mods/tools/tools.lua
@@ -1,5 +1,42 @@
 -- tools/tools.lua
 
+tools.register("Steel", "steel", {
+	material = "ores:iron",
+	pick = {"tools_steel_pick.png", {
+		full_punch_interval = 1.0,
+		max_drop_level=1,
+		groupcaps={
+			cracky = {times={[1]=4.00, [2]=1.60, [3]=0.80}, uses=20, maxlevel=2},
+		},
+		damage_groups = {fleshy=4},
+	}},
+	axe = {"tools_steel_axe.png", {
+		full_punch_interval = 1.0,
+		max_drop_level=1,
+		groupcaps={
+			choppy={times={[1]=2.50, [2]=1.40, [3]=1.00}, uses=20, maxlevel=2},
+		},
+		damage_groups = {fleshy=4},
+	}},
+	shovel = {"tools_steel_shovel.png", {
+		full_punch_interval = 1.1,
+		max_drop_level=1,
+		groupcaps={
+			crumbly = {times={[1]=1.50, [2]=0.90, [3]=0.40}, uses=30, maxlevel=2},
+		},
+		damage_groups = {fleshy=3},
+	}},
+	hoe = {"tools_steel_hoe.png"},
+	sword = {"tools_steel_sword.png", {
+		full_punch_interval = 0.8,
+		max_drop_level=1,
+		groupcaps={
+			snappy={times={[1]=2.5, [2]=1.20, [3]=0.35}, uses=30, maxlevel=2},
+		},
+		damage_groups = {fleshy=6},
+	}}
+})
+
 tools.register("Diamond", "diamond", {
 	material = "ores:diamond",
 	pick = {"tools_diamond_pick.png", {
@@ -31,19 +68,19 @@ tools.register("Diamond", "diamond", {
 tools.register("Wooden", "wood", {
 	material = "group:plank",
 	pick = {"tools_wood_pick.png", {
-		full_punch_interval = 0.9,
-		max_drop_level=3,
+		full_punch_interval = 1.2,
+		max_drop_level=0,
 		groupcaps={
-			cracky = {times={[1]=2.0, [2]=4.0, [3]=6.0}, uses=30, maxlevel=3},
+			cracky = {times={[3]=1.60}, uses=10, maxlevel=1},
 		},
 		damage_groups = {fleshy=2},
 	}},
 	axe = {"tools_wood_axe.png", {
-		full_punch_interval = 0.9,
-		max_drop_level=1,
+		full_punch_interval = 1.0,
+		max_drop_level=0,
 		groupcaps={
-			choppy={times={[1]=2.10, [2]=0.90, [3]=0.50}, uses=30, maxlevel=2},
+			choppy = {times={[2]=3.00, [3]=1.60}, uses=10, maxlevel=1},
 		},
-		damage_groups = {fleshy=7},
+		damage_groups = {fleshy=2},
 	}},
 })
