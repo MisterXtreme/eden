828dc6ef430545946c0202eb54a7b8095adf8ac7
octacian <enduffy2014@outlook.com>
2017-07-24 21:27:59 -0700

    Ores: Add diamond/coal lumps, gold/iron ingots * Add coal lump * Add diamond "lump" * Add gold ingot * Add iron ingot * Add lump + wherein drop mechanism

--- mods/ores/init.lua
+++ mods/ores/init.lua
@@ -31,12 +31,50 @@ function ores.register(name, def)
 		register_oredef("ores:"..name, def)
 	end
 
+	local drop = "ores:"..name
+	if def.lump then
+		drop = "ores:"..name.."_lump"
+
+		if def.drop_wherein ~= false then
+			drop = {
+				max_items = 1,
+				items = {
+					{
+						items = {drop, def.ore_wherein},
+						rarity = 2,
+					},
+					{
+						items = {drop},
+						rarity = 1,
+					},
+				}
+			}
+		end
+	end
+
 	-- Ore node
 	minetest.register_node("ores:"..name, {
 		description = def.basename.." Ore",
 		tiles = {"eden_stone.png^"..def.texture},
-		groups = def.groups
+		groups = def.groups,
+		drop = drop,
 	})
+
+	-- Ore Lump
+	if def.lump then
+		minetest.register_craftitem("ores:"..name.."_lump", {
+			description = def.lump_description or def.basename.." Lump",
+			inventory_image = def.lump,
+		})
+	end
+
+	-- Ingot
+	if def.ingot then
+		minetest.register_craftitem("ores:"..name.."_ingot", {
+			description = def.ingot_description or def.basename.." Ingot",
+			inventory_image = def.ingot,
+		})
+	end
 end
 
 ---
@@ -47,6 +85,7 @@ end
 ores.register("coal", {
 	basename = "Coal",
 	texture = "ores_coal_ore.png",
+	lump = "ores_coal_lump.png",
 	groups = {cracky = 3},
 	ore_type = "scatter",
 	ore_wherein = "eden:stone",
@@ -72,6 +111,7 @@ ores.register("coal", {
 ores.register("iron", {
 	basename = "Iron",
 	texture = "ores_iron_ore.png",
+	ingot = "ores_iron_ingot.png",
 	groups = {cracky = 2},
 	ore_type = "scatter",
 	ore_wherein = "eden:stone",
@@ -97,6 +137,7 @@ ores.register("iron", {
 ores.register("gold", {
 	basename = "Gold",
 	texture = "ores_gold_ore.png",
+	ingot = "ores_gold_ingot.png",
 	groups = {cracky = 2},
 	ore_type = "scatter",
 	ore_wherein = "eden:stone",
@@ -122,6 +163,8 @@ ores.register("gold", {
 ores.register("diamond", {
 	basename = "Diamond",
 	texture = "ores_diamond_ore.png",
+	lump = "ores_diamond_lump.png",
+	lump_description = "Diamond",
 	groups = {cracky = 1, level = 3},
 	ore_type = "scatter",
 	ore_wherein = "eden:stone",
